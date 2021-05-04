2d987e57f9a460e822609b882f80999e9196ed72
BrenoVale <brenovale8@gmail.com>
2021-05-03 21:08:22 -0300

    ProjetoIgNews

---
+++ services/prismic.ts
@@ -0,0 +1,14 @@
+import Prismic from '@prismicio/client'
+
+
+export function getPrismicClient(req?: unknown) {
+    const prismic = Prismic.client(
+        process.env.PRISMIC_ENDPOINT,
+        {
+            req: req,
+            accessToken: process.env.PRISMIC_ACESS_TOKEN
+        }
+    )
+
+    return prismic;
+}
